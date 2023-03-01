77c863550f80c35ede2e5362a9453f3ab97d5643
Minttu Hillebrandt <minttu.hillebrandt@edu.keuda.fi>
2023-03-01 13:05:53 +0200

    -

--- demo7/kirjautuminen.js
+++ demo7/kirjautuminen.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", onkoKirjautunut);
 function onkoKirjautunut(){
 let kirjautunut = 'kylla';
 if (kirjautunut === 'kylla') {
-    document.getElementById('tervetulo_teksti').textContent += localStorage.getItem(" nimi");
+    document.getElementById('tervetulo_teksti').textContent += localStorage.getItem("nimi");
     }
 }
 
