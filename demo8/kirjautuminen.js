ccbc7f9bc21d81538aa6c5767d367e19f84f7a70
Minttu Hillebrandt <minttu.hillebrandt@edu.keuda.fi>
2023-03-01 13:30:43 +0200

    -

--- demo8/kirjautuminen.js
+++ demo8/kirjautuminen.js
@@ -10,4 +10,6 @@ if (localStorage.getItem(kirjautunut) === 'kylla') {
 function kirjaudu(){
     localStorage.setItem("nimi", document.getElementById("nimi").value)
     localStorage.setItem("kirjautunut","kylla")
+
+    document.getElementById('kirjauduulos_lomake').textContent += localStorage.getItem("nimi");
 }
